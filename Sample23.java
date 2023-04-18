563530bc6e3163d874181c4f690281481ff7361b
219075331 <110978849+219075331@users.noreply.github.com>
2023-04-19 01:27:13 +0900

    演習２３

---
+++ Sample23.java
@@ -0,0 +1,18 @@
+
+public class Sample23 {
+
+	public static void main(String[] args) {
+		
+		String string = "1000";
+		int bdM = 4;
+		
+		int StringtoInt = Integer.parseInt(string);
+		int cal = bdM + StringtoInt;
+		
+		System.out.println(cal);
+		
+		
+
+	}
+
+}
