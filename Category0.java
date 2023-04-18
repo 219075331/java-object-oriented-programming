2cc4deab003dc050456f53536e10e1babd982840
219075331 <110978849+219075331@users.noreply.github.com>
2023-04-19 01:28:24 +0900

    演習２５

---
+++ Category0.java
@@ -0,0 +1,33 @@
+
+class Category0 {
+	
+	private String id;
+	private String name;
+	
+	public Category0() {
+		
+	}
+	
+	public Category0(String id, String name) {
+		this.setId(id);
+		this.setName(name);
+		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
